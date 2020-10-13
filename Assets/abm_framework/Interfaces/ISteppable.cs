93063f219274a52ac8e7ec41e462954f00ac57ee
cheliotk <cheliotis.k@gmail.com>
2020-10-13 21:43:38 +0300

    ISteppable implementation

--- Assets/abm_framework/Interfaces/ISteppable.cs
+++ Assets/abm_framework/Interfaces/ISteppable.cs
@@ -7,21 +7,9 @@
         /// </summary>
         public interface ISteppable
         {
-            /// <summary>
-            /// Default step method. Executes all Steppers registered by the implementing class within a specific stepperQueue slot (EARLY, NORMAL, LATE) 
-            /// </summary>
-            /// <param name="_stepperQueuePrompt">The stepperQueue value slot to filter steppers by</param>
-            void Step(Stepper.StepperQueueOrder _stepperQueuePrompt);
 
             /// <summary>
-            /// Default step method. Executes all Steppers registered by the implementing class within a specific stepper priority range (0-1000, lower is earlier) 
-            /// </summary>
-            /// <param name="priorityS">The lower bound for priority range (inclusive)</param>
-            /// <param name="priorityE">The upper bound for priority range (exclusive)</param>
-            void Step(int priorityS, int priorityE);
-
-            /// <summary>
-            /// Default step method. Executes all Steppers registered by the implementing class
+            /// Default step method. Executes the corresponding method registered by the implementing class
             /// </summary>
             void Step();
         }
